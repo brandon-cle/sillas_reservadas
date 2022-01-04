c559cdbec5a06e7e1a09302bd9e340200bf182ef
brandon-cle <leninbrandon805@gmail.com>
2022-01-04 05:46:21 -0600

    Started forms to work.

---
+++ ChallengeSaul/Frm_Brandon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChallengeSaul
+{
+    public partial class Frm_Brandon : Form
+    {
+        public Frm_Brandon()
+        {
+            InitializeComponent();
+        }
+    }
+}
