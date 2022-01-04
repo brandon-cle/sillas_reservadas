44b90c2edb285a10d4fd19db4c30a04fc1bce124
brandon-cle <leninbrandon805@gmail.com>
2022-01-04 15:09:13 -0600

    Collatz finished

--- ChallengeSaul/Model/Collatz.cs
+++ ChallengeSaul/Model/Collatz.cs
@@ -14,11 +14,10 @@ namespace ChallengeSaul.Model
         int impar_count;
         int count = 0;
         //Data to graph
-        List<int> x_axis = new List<int>();
         List<int> y_axis = new List<int>();
         //////////////////////////////////////
         
-        //Function to giving a value to the object attributes
+        //Function to giving a value to the object attributes 
         public void number_collatz(int n)
         {
             
@@ -32,7 +31,6 @@ namespace ChallengeSaul.Model
             number = n;
             //First number to start
             y_axis.Add(n);
-            x_axis.Add(count);
             while (n != 1)
             {
                 if (n % 2 == 0)
@@ -48,11 +46,13 @@ namespace ChallengeSaul.Model
 
                 count = count + 1;
                 y_axis.Add(n);
-                x_axis.Add(count);
             }
 
         }
 
-
+        public List<int> Give_List()
+        {
+            return y_axis;
+        }
     }
 }
